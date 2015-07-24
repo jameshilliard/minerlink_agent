738a91baf47c97a3ca0ed178d49f0085bf6c833a
fyw <olympic0324@163.com>
2015-07-24 16:07:20 +0800

    code of minermonitor

---
+++ config.h
@@ -0,0 +1,14 @@
+#ifndef  __CONFIG_H_
+#define  __CONFIGH_
+
+//#define OPENWRT   //S3,R1...
+#define BB_BLACK    //S4,S5,S4+,S5+...
+
+#ifndef MINERTYPE
+#define MINERTYPE 500 //s5 default
+#endif // MINERTYPE
+#ifndef ETH
+#define ETH "eth0"
+#endif // ETH
+
+#endif
